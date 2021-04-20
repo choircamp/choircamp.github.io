286c4c5c80e63d6aa1f63e807d7607497067744f
Lance Hilliard <lance@digitalcreations.cc>
2021-04-19 20:55:04 -0500

    postpone 2021 ☹️

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
-var campYear = "2021";
-var campZeroDay = "July 2021";
-var campFirstDay = "July 2021";
-var campLastDay = "July 2021";
+var campYear = "2022";
+var campZeroDay = "July "+campYear;
+var campFirstDay = "July "+campYear;
+var campLastDay = "July "+campYear;
 var directorFirstName = "Charlie";
 var directorLastName = "Rigsby";
 var directorAddress1 = "1011 Country Club Road";
