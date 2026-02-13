614b9fe18c6809759549d677726908cc60474de2
Lance Hilliard <lance@digitalcreations.cc>
2026-02-12 23:05:31 -0600

    add sender.net form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,4 +28,20 @@ var registrationFormsAddressLine1 = campContactAddressLine1;
 var registrationFormsAddressLine2 = campContactAddressLine2;
 var registrationFormsAddressCity = campContactAddressCity;
 var registrationFormsAddressState = campContactAddressState;
-var registrationFormsAddressZip = campContactAddressZip;
+var registrationFormsAddressZip = campContactAddressZip;var registrationFormsAddressZip = campContactAddressZip;
+
+
+// https://app.sender.net/forms/dL9zy4/settings (related: newsletter/index.html)
+  (function (s, e, n, d, er) {
+    s['Sender'] = er;
+    s[er] = s[er] || function () {
+      (s[er].q = s[er].q || []).push(arguments)
+    }, s[er].l = 1 * new Date();
+    var a = e.createElement(n),
+        m = e.getElementsByTagName(n)[0];
+    a.async = 1;
+    a.src = d;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+  sender('7052d128d76544')
+///////////////////////////////////////////////
